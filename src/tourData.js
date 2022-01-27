e1b12b8d12a34007534778d824518a7624e4ac90
Nelson Lucas <nelsonlucas@MacBook-Pro-de-Nelson.local>
2022-01-27 16:46:25 -0300

    first commit

---
+++ src/tourData.js
@@ -0,0 +1,35 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.jpeg",
+    name: "new york bridge tour",
+    info:
+      "Come see the beauty and history of Lower Manhattan and the Brooklyn Bridge at an easy pace with one of New York's best tour guides. This walk will make you an expert on New York history and a lover of its stunning views. You'll get a good view of the Statue of Liberty, The Sphere - a sculpture that once honored the World Trade Center - and the New York Stock Exchange. This tour is two hours. (Note: This tour does not take you to the National 9/11 Memorial). The New York Pass provides FREE entry."
+  
+    },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpeg",
+    name: "paris lights tour",
+    info:
+      "After the sun sets in the City of Love, Paris transforms into the City of Lights. Discover the French capital's top attractions by night on a 2-hour evening walking tour. After the tourist hordes subside, follow your guide to the Eiffel Tower, Arc de Triomphe, Grand Palais, and more. Hear stories of love, royal scandal and revolution as you see the illuminated monuments."
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpeg",
+    name: "london royal palace tour",
+    info:
+      "An iconic building and official residence of The Queen, Buckingham Palace has been the focus of many moments of national celebration, from Jubilees and weddings to VE Day and the annual Trooping the Color which marks The Queen's official birthday. It is also a busy working building welcoming you have of thousands of people through its gates each year for Investments, Garden Parties, Audiences and other events."
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo sushi tour",
+    info:
+      "Ever fancied learning how to make sushi on your own? Here, over 3,000 students so far have learned to make their sushi through the teaching of the art from an expert here. There are two types of tours: the independent tour (up to 10 people) and group tour (up to 60 people). During the tour, you will learn the history of sushi, try making different types and practice sushi etiquette. It is one of the most memorable hands-on Japanese experiences."
+  }
+];
